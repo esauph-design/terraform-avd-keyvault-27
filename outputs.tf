1fe50b55d4f64493b3cb59ecf32a5de18393cad4
Esau Picado <esauph@gmail.com>
2026-08-14 10:36:30 -0600

    added key_vault_id to outputs for usage in module 30

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "key_vault_name" {
   description = "Azure Key Vault name"
   value       = azurerm_key_vault.key_vault.name
 }
+output "key_vault_id" {
+  description = "Resource ID of the Azure Key Vault."
+  value       = azurerm_key_vault.key_vault.id
+}
